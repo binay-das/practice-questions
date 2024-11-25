bc5c33134be38e0cca7dc6318465eb27f4b57bdb
Binay <binaydas1123@gmail.com>
2024-11-25 15:53:29 +0530

    If a number can be represented as a power of 2

---
+++ leetcode/easy/Q231.java
@@ -0,0 +1,24 @@
+package leetcode.easy;
+
+public class Q231 {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(32));
+    }
+
+    // 231. Power of Two
+    // Given an integer n, return true if it is a power of two. Otherwise, return false.
+    // An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+    public static boolean isPowerOfTwo(int n) {
+        int num = 1;
+        for (int i = 0; i <= 30; i++) {
+            if (num == n) {
+                return true;
+            }
+            if (num < Integer.MAX_VALUE / 2) {
+                num = num * 2;
+            }
+        }
+        return false;
+    }
+}
