a453dd5483ef3fbad61f39dd5a68c42326a12d4b
Binay <binaydas1123@gmail.com>
2024-11-25 14:34:59 +0530

    Reverse Integer

--- leetcode/medium/Q7.java
+++ leetcode/medium/Q7.java
@@ -10,13 +10,26 @@ public class Q7 {
     //Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 
     public static int reverse(int x) {
-        int ans = 0;
+        int reverse = 0;
         while (x != 0) {
-            ans = ans * 10 + x % 10;
+            int rem = x % 10;
+            if (reverse > Integer.MAX_VALUE / 10 || reverse == Integer.MAX_VALUE && rem > 7) {
+                return 0;   
+                
+                // if reverse > Integer.MAX_VALUE / 10, multiplying it by 10 will cause overflow,
+                // now if reverse = Integer.MAX_VALUE / 10, then the rem to be added should not be greater than 7, otherwise overflow will occur
+            }
+            if (reverse < Integer.MIN_VALUE / 10 || reverse == Integer.MIN_VALUE && rem < -8) {
+
+                // if reverse < Integer.MIN_VALUE / 10, multiplying it by 10 will cause overflow,
+                // now if reverse = Integer.MIN_VALUE / 10, then the rem to be added should not be lesser than -8, otherwise overflow will occur
+                return 0;
+            }
+            reverse = reverse * 10 + rem;
             x /= 10;
         }
 
-        return ans;
+        return reverse;
     }
 
 }
