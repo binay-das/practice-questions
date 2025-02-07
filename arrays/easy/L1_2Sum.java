2cd34d2ab0e138d9be2cc50c664379538304dd67
Binay <binaydas1123@gmail.com>
2025-02-07 08:52:02 +0530

    2 Sum Array

---
+++ arrays/easy/L1_2Sum.java
@@ -0,0 +1,27 @@
+package arrays.easy;
+
+// L 1. Two Sum
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//You can return the answer in any order.
+
+import java.util.Arrays;
+
+public class L1_2Sum {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
